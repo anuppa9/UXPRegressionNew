a7e6114c24723330dee92a949fab70c76af9e86c
Anup <119912665+anuppa9@users.noreply.github.com>
2023-10-20 15:53:58 +0530

    Basic Framework Structure

---
+++ src/main/java/AndMore/UXPRegressionNew/App.java
@@ -0,0 +1,13 @@
+package AndMore.UXPRegressionNew;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
